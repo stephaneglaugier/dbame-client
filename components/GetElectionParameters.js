66bd3190066163532aa96f063e7eb82e99037299
Stephane Augier <stephane@augiermail.com>
2023-03-24 11:50:04 +0000

    error handling

--- components/GetElectionParameters.js
+++ components/GetElectionParameters.js
@@ -12,8 +12,10 @@ const GetElectionParameters = ({ navigation }) => {
     const context = React.useContext(DBAMEContext);
 
     const [response, setResponse] = useState('');
+    const [error, setError] = useState('');
 
     const handleSubmit = async () => {
+        setError('');
         try {
             const requestOptions = {
                 headers: {
@@ -22,27 +24,34 @@ const GetElectionParameters = ({ navigation }) => {
                 }
             };
             const response = await fetch(`${context.domain}/registrar/electionParams`, requestOptions)
+            console.log(response);
             const json = await response.json();
-            console.debug(response.status);
-            console.debug(json);
-            context.setDbameVersion(json.dbameVersion);
-            context.setP(json.p);
-            context.setG(json.g);
-            context.setIv(json.iv);
-            context.setCandidates(json.candidates);
-            context.setVotes(new Array(32).fill(false));
-            context.setContractAddress(json.contractAddress);
-            context.setContractNetwork(json.contractNetwork);
-            context.setVotingNode(json.votingNode);
-            context.setVotingClient(json.votingClient);
-            context.setElectionState(json.electionState);
-            context.setYR(json.yR);
-            context.setYM(json.yM);
-
-            setResponse(json);
+            if (response.ok) {
+                console.debug(response.status);
+                console.debug(json);
+                context.setDbameVersion(json.dbameVersion);
+                context.setP(json.p);
+                context.setG(json.g);
+                context.setIv(json.iv);
+                context.setCandidates(json.candidates);
+                context.setVotes(new Array(32).fill(false));
+                context.setContractAddress(json.contractAddress);
+                context.setContractNetwork(json.contractNetwork);
+                context.setVotingNode(json.votingNode);
+                context.setVotingClient(json.votingClient);
+                context.setElectionState(json.electionState);
+                context.setYR(json.yR);
+                context.setYM(json.yM);
+                setResponse(json);
+            } else {
+                console.error(`Error: ${response.status} ${json.message}`);
+                setError(`${response.status}: ${json.message}`);
+            }
+            
         }
         catch (error) {
             console.error(error);
+            setError(error.message);
         }
     }
 
@@ -51,6 +60,11 @@ const GetElectionParameters = ({ navigation }) => {
             <View style={globalStyles.container}>
                 <InputBox name={"Domain:"} inputText={context.domain} setInputText={context.setDomain}></InputBox>
                 <CustomButton onPress={handleSubmit} title="Retrieve" />
+                {error ? (
+                    <View>
+                        <Text style={globalStyles.errorText}>{error}</Text>
+                    </View>
+                ) : null}
                 <View style={globalStyles.response}>
                     <JSONText data={response} />
                 </View>
